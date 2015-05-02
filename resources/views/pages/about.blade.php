2a780771db5bcf9e97f66a2ddba94d4ceef3ad4c
slackerzz <lorenzost@gmail.com>
2015-05-03 00:09:29 +0200

    lesson 4

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>About Me {{ $first }}</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti dicta dolore dolorum ex ipsam, laborum, nemo porro quaerat quasi quisquam repellendus soluta veritatis! Culpa doloremque enim hic provident rem.
+    </p>
+</body>
+</html>
