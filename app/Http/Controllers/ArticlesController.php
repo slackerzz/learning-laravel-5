35dbb1dc4cbbe4ff8e1b4d2db551eb00d40e65f7
slackerzz <lorenzost@gmail.com>
2015-05-03 17:42:34 +0200

    lesson 9

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,28 @@
+<?php namespace App\Http\Controllers;
+
+use App\Article;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller {
+
+	public function index()
+    {
+        $articles = Article::all();
+
+        return view('articles.index', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        //dd($article);
+        if (is_null($article)) {
+            abort(404);
+        }
+        return view('articles.show', compact('article'));
+
+    }
+}
