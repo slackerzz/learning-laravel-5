408462dfad52a2c24b0cea2496dc01b98e6c080c
slackerzz <lorenzost@gmail.com>
2015-05-06 15:27:38 +0200

    phpDocBlock

--- app/Article.php
+++ app/Article.php
@@ -3,6 +3,26 @@
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Article
+ *
+ * @property integer $id 
+ * @property string $title 
+ * @property string $body 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property \Carbon\Carbon $published_at 
+ * @property string $excerpt 
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article wherePublishedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereExcerpt($value)
+ * @method static \App\Article published()
+ * @method static \App\Article unpublished()
+ */
 class Article extends Model {
 
     // attributes that can be massAssigned
--- app/Task.php
+++ app/Task.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Task
+ *
+ * @property integer $id 
+ * @property string $name 
+ * @property string $description 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereUpdatedAt($value)
+ */
 class Task extends Model {
 
 	protected $fillable = [
